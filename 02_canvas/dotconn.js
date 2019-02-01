3b8b3df5571aeb9511f6962be5d9da157ed20b6d
Kaitlin <kaitlin.wan@gmail.com>
2019-02-01 12:32:37 -0500

    lined

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -1,7 +1,7 @@
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d")
 var drawn = false;
-
+var prevX, prevY;
 
 var butt= document.getElementById("clear")
 
@@ -23,10 +23,28 @@ else{
 // x = br.left
 // y = br.top
 c.addEventListener('click', function(e){
+  if (drawn == false){
     ctx.fillStyle = "green"
     //prevents accumulation of pathways (i.e., clicking three spots wont create triangle outline)
     ctx.beginPath();
     ctx.ellipse(e.offsetX ,e.offsetY, 15,15, 0,0, 2 * Math.PI);
     ctx.fill();
+    console.log(e.offsetX, e.offsetY)
+    prevX = e.offsetX
+    prevY = e.offsetY
     drawn = true;
+  }
+  else{
+    ctx.beginPath();
+    ctx.ellipse(e.offsetX ,e.offsetY, 15,15, 0,0, 2 * Math.PI);
+    ctx.fill();
+    ctx.lineTo(prevX,prevY)
+    ctx.stroke();
+
+    prevX = e.offsetX
+    prevY = e.offsetY
+
+
+
+  }
   })
