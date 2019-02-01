bf747928e6acd32dd36268d1fc13da04af01770d
Kaitlin <kaitlin.wan@gmail.com>
2019-02-01 03:52:02 -0500

    fix

--- 01_canvas/clickdraw.js
+++ 01_canvas/clickdraw.js
@@ -7,6 +7,7 @@ var draw = document.getElementById("draw")
 draw.addEventListener('click', function(e) {
   ctx.fillStyle = "blue";
   ctx.fillRect(0,0,c.width,c.height);
+  drawn = true;
 })
 
 var butt= document.getElementById("clear")
@@ -18,6 +19,7 @@ butt.addEventListener('click', function(e) {
   drawn = false;
 }
 else{
+  //prevents default function:wont clear if nothing to clear
   e.preventDefault()
   console.log("Can't clear! Nothing to be cleared!")
 }
@@ -29,6 +31,8 @@ var dot = 0;
 var toggle = document.getElementById("toggle")
 
 toggle.addEventListener('click', function(e) {
+//prevents the default function from being carried out
+e.preventDefault();
 if(dot==0){
   dot = 1;
   toggle.innerHTML = "Dot"
@@ -48,11 +52,13 @@ else{
 c.addEventListener('click', function(e){
   if(dot==0){
     ctx.fillStyle = "red";
+    //offset coordinates fetch x and y coordinates of mouse relative to canvas border
     ctx.fillRect(e.offsetX, e.offsetY, 30,30);
     drawn = true;
   }
   if(dot==1){
     ctx.fillStyle = "green"
+    //prevents accumulation of pathways (i.e., clicking three spots wont create triangle outline)
     ctx.beginPath();
     ctx.ellipse(e.offsetX ,e.offsetY, 15,15, 0,0, 2 * Math.PI);
     ctx.fill();
