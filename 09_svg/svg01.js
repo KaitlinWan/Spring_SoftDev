696e2665b4f6d780dfe17546dc3f767a59ab61ea
Kaitlin <kaitlin.wan@gmail.com>
2019-03-13 00:16:42 -0400

    .

---
+++ 09_svg/svg01.js
@@ -0,0 +1,45 @@
+//Kaitlin Wan
+//SoftDev2 pd6
+//K09 -- Connect the Dots
+//2019-03-13w
+
+var p = document.getElementById("vimage");
+var c = document.getElementById("but_clear");
+prev = 0;
+
+var draw = function(e){
+
+  var dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  dot.setAttribute("cx", e.offsetX);
+  dot.setAttribute("cy", e.offsetY);
+  dot.setAttribute("r", 10);
+  dot.setAttribute("fill", "red");
+  dot.setAttribute("stroke", "black");
+  p.appendChild(dot)
+
+  if (prev != 0){
+      var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      line.setAttribute("x1", prev.getAttribute( "cx" ) );
+      line.setAttribute("y1", prev.getAttribute("cy") );
+      line.setAttribute("x2", e.offsetX);
+      line.setAttribute("y2", e.offsetY);
+      line.setAttribute("stroke", "black");
+      p.appendChild(line);
+    }
+
+    // Prev holds dot just added
+    prev = dot
+};
+
+
+var clear = function(e) {
+  while (p.lastChild) {
+    p.removeChild(p.lastChild);
+  }
+  prev = 0
+};
+
+
+
+p.addEventListener('click', draw);
+c.addEventListener("click", clear);
