174c1aae64321dad3c6fdebaf207a678b7a46f63
Kaitlin <kaitlin.wan@gmail.com>
2019-03-13 12:03:18 -0400

    color

--- 09_svg/svg01.js
+++ 09_svg/svg01.js
@@ -12,8 +12,8 @@ var draw = function(e){
   var dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   dot.setAttribute("cx", e.offsetX);
   dot.setAttribute("cy", e.offsetY);
-  dot.setAttribute("r", 10);
-  dot.setAttribute("fill", "red");
+  dot.setAttribute("r", 15);
+  dot.setAttribute("fill", "magenta");
   dot.setAttribute("stroke", "black");
   p.appendChild(dot)
 
