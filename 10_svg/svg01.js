4f3c94063f993aa93bf9b42906ca579b581180c2
Kaitlin <kaitlin.wan@gmail.com>
2019-03-18 02:32:22 -0400

    changes

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -1,6 +1,6 @@
 //Kaitlin Wan
 //SoftDev2 pd6
-//K09 -- Connect the Dots
+//K10 -- Connect the Dots
 //2019-03-13w
 
 var p = document.getElementById("vimage");
@@ -12,20 +12,20 @@ var draw = function(e){
   var dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   dot.setAttribute("cx", e.offsetX);
   dot.setAttribute("cy", e.offsetY);
-  dot.setAttribute("r", 5);
+  dot.setAttribute("r", 20);
   dot.setAttribute("fill", "magenta");
   dot.setAttribute("stroke", "black");
   p.appendChild(dot)
 
-  if (prev != 0){
-      var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-      line.setAttribute("x1", prev.getAttribute( "cx" ) );
-      line.setAttribute("y1", prev.getAttribute("cy") );
-      line.setAttribute("x2", e.offsetX);
-      line.setAttribute("y2", e.offsetY);
-      line.setAttribute("stroke", "black");
-      p.appendChild(line);
-    }
+  //if (prev != 0){
+    //  var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      //line.setAttribute("x1", prev.getAttribute( "cx" ) );
+      //line.setAttribute("y1", prev.getAttribute("cy") );
+      //line.setAttribute("x2", e.offsetX);
+      //line.setAttribute("y2", e.offsetY);
+      //line.setAttribute("stroke", "black");
+      //p.appendChild(line);
+    //}
 
     // Prev holds dot just added
     prev = dot
