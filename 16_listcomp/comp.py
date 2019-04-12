1a7a3063d79dfe53bf7ccd3ccd0014fe2d9336f3
Kaitlin <kaitlin.wan@gmail.com>
2019-04-12 12:01:39 -0400

    omg i never pushed it :(((

--- 16_listcomp/comp.py
+++ 16_listcomp/comp.py
@@ -1,20 +1,21 @@
-#Kaitlin Wan
-#SoftDev2 pd6
-#K#16 - Do You Even List?
+# Kaitlin Wan
+# SoftDev2 pd6
+# K#16 - Do You Even List?
+
 
 def checkPass1(lala):
     UC_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     UC_LOWER = UC_UPPER.lower()
-    NUMS = ['0','1','2','3','4','5','6','7','8','9']
+    NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     case1 = False
     case2 = False
     case3 = False
-    #One of Each
+    # One of Each
     if len([1 for x in lala if x in UC_UPPER]) > 1:
         case1 = True
     if len([1 for x in lala if x in UC_LOWER]) > 1:
         case2 = True
-    if len([1 for x in lala if x in NUMS]) >1:
+    if len([1 for x in lala if x in NUMS]) > 1:
         case3 = True
     return (case1 and case2 and case3)
 
@@ -25,21 +26,21 @@ print(checkPass1('He'))
 print(checkPass1('eee'))
 
 
-#Write a function that uses list comprehension to return a
-#password's strength rating. This function should return a
-#lower integer for a weak password and a higher integer for
-#a stronger password. (Suggested scale: 1-10) Consider these criteria:
+# Write a function that uses list comprehension to return a
+# password's strength rating. This function should return a
+# lower integer for a weak password and a higher integer for
+# a stronger password. (Suggested scale: 1-10) Consider these criteria:
 
-#mixture of upper- and lower-case
-#inclusion of numerals
-#inclusion of these non-alphanumeric chars: . ? ! & # , ; : - _ *
+# mixture of upper- and lower-case
+# inclusion of numerals
+# inclusion of these non-alphanumeric chars: . ? ! & # , ; : - _ *
 
 def ratePass(p):
-    rating= 0
+    rating = 0
     UC_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     UC_LOWER = UC_UPPER.lower()
-    NUMS = ['0','1','2','3','4','5','6','7','8','9']
-    special = [ '.', '?', '!' ,'&' ,'#', ',' ,';' ,':' ,'-', '_', '*']
+    NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    special = ['.', '?', '!', '&', '#', ',', ';', ':', '-', '_', '*']
     if len(p) < 6:
         return 0
 
